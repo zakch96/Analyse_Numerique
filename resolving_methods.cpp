8b6b6d4157852ee6b6d7b2aca35f9799da8d2f06
an13001 <37494427+an13001@users.noreply.github.com>
2018-11-19 11:07:55 -0500

    Update resolving_methods.cpp

--- resolving_methods.cpp
+++ resolving_methods.cpp
@@ -1,1 +1,34 @@
 
+float* Grad_Conjug(float** A, float* b, float* x_k, int n)
+{
+  float* r_k = new float[n];
+  float* p_k = new float[n];
+  float* r_k_1 = new float[n];
+  float* x_k_1 = new float[n];
+  float* p_k_1 = new float[n];
+  float* prod_mat_vec = Prod_Mat_Vec(A, x_k, n);
+  for(int i=0; i<n; i++)
+    r_0[i] = b[i] - prod_mat_vec[i];
+  for(int i=0; i<n; i++)
+    p_k[i] = r_k[i];
+  int k = 0;
+  float alpha_k;
+  float beta_k;
+  while(1){
+    alpha_k = Prod_Vec(r_k, r_k, n)/Pro_Vec(p_k, Prod_Mat_Vec(A, p_k, n), n);
+    for(int i=0; i<n; i++){
+      x_k_1[i] = x_k[i] + alpha_k*p_k[i];
+      r_k_1[i] = r_k[i] + alpha_k*p_k[i];
+    }
+    if("r_k_1 est suffisamment petit")
+      return x_k_1;
+    beta_k = Prod_Vec(r_k_1, r_k_1, n)/Prod_Vec(r_k, r_k, n);
+    for(int i=0; i<n; i++)
+      p_k_1[i] = r_k_1[i] + beta_k*p_k[i];
+    for(int i=0; i<n; i++){
+      r_k[i] = r_k_1[i];
+      p_k[i] = p_k_1[i];
+    }
+  }
+}
+
