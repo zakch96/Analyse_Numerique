432c35ed388c0c64decf58ba771e1b182365ad9a
Anas Naddami <37494427+an13001@users.noreply.github.com>
2018-11-20 11:50:44 -0500

    Update matrix_creation.cpp

--- matrix_creation.cpp
+++ matrix_creation.cpp
@@ -27,3 +27,22 @@ float** Create_A(int n, int m, float h_sqr, float k_sqr, float d, int dim)
     count++;
   }
 }
+
+
+
+
+float* Create_b(int n, int m, float a, float b, float sqr_k, float sqr_h, int dim)
+{
+  float* b = new float[dim];
+  for(int i=0; i<m; i++)
+    for(int j=0; j<n; j++)
+      b[i + (j-1)*m] = f(i*h, j*k);
+  for(int i>0; i<(m-1); i++){
+    b[i+(n-1)*m] += g(i*h, b)/sqr_k;
+    b[i] += g(i*h, 0)/sqr_k;
+  }
+  for(int j>0; j<(n-1); j++){
+    b[1+(j-1)*n] += g(0, j*k)/sqr_h;
+    b[m+(j-1)*n] += g(a, j*k)/sqr_h;
+  }
+}
