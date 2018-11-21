d89d81d7d83d655479b2da35854c62f633e62817
Anas Naddami <37494427+an13001@users.noreply.github.com>
2018-11-21 11:23:37 -0500

    Update matrix_creation.cpp

--- matrix_creation.cpp
+++ matrix_creation.cpp
@@ -1,34 +1,35 @@
-float** Create_A(int n, int m, float h_sqr, float k_sqr, float d, int dim)
+
+double** Create_A(int n, int m, double h_sqr, double k_sqr, double d)
 {
-  float** A = new float*[dim];
-  for(int i=0; i<m; i++)
-    A[i] = new float[dim];
+  int dim = n*m;
+  double** A = new double*[dim];
+  for(int i=0; i<dim; i++)
+    A[i] = new double[dim];
   for(int i=0; i<dim; i++){
     A[i][i] = d;
-    if(1=<i)
+    if(1<=i)
       A[i][i-1] = -1/h_sqr;
     if(i < dim-1)
       A[i][i+1] = -1/h_sqr;
-    if(n=<i)
+    if(n<=i)
       A[i][i-n] = -1/k_sqr;
     if(i<dim-n)
       A[i][i+n] = -1/k_sqr;
   }
-  int count = 0;
+  int count_1 = 0;
   for(int i=m; i<dim-m+1; i++){
-    if((cout % m) == 0)
+    if((count_1 % m) == 0)
       A[i][i-1] = 0;
-    count++;
+    count_1++;
   }
-  count = 0;
+  count_1 = 0;
   for(int i=dim-m-1; i>m-2; i--){
-    if((cout % m) == 0)
+    if((count_1 % m) == 0)
       A[i][i+1] = 0;
-    count++;
+    count_1++;
   }
   return A;
-}
-
+} 
 
 
 
